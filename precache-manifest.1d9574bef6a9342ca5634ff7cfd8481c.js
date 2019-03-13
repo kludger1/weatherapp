f9ea3dd2a1a907ffe75df5bcea4f64f172aeae7b
unknown <ludgerk@techlaunch.com>
2019-03-13 00:15:19 -0400

    first commit

---
+++ precache-manifest.1d9574bef6a9342ca5634ff7cfd8481c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acd38d9746f8904dbaa2",
+    "url": "weatherapp/static/css/main.908f09bc.chunk.css"
+  },
+  {
+    "revision": "acd38d9746f8904dbaa2",
+    "url": "weatherapp/static/js/main.977a453b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "weatherapp/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "95e129707b6bb464a478",
+    "url": "weatherapp/static/js/2.fcbb77aa.chunk.js"
+  },
+  {
+    "revision": "53c87369e3dbc4ffaf398c875498cbac",
+    "url": "weatherapp/index.html"
+  }
+];
